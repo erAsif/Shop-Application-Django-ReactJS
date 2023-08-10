124534ba68f6434d00596e80eddd6cd7f22a2b1e
erAsif <106155182+erAsif@users.noreply.github.com>
2023-08-11 00:21:26 +0530

    production_allow_host

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["shop.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS",
+                        #   default=["shop.com"]
+                          default=["shop.com","shop-app-n2c3.onrender.com"]
+                          )
 
 # DATABASES
 # ------------------------------------------------------------------------------
