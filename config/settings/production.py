4fa36da702fc4e6b14ead6f6034b25e5779f2e19
erAsif <106155182+erAsif@users.noreply.github.com>
2023-08-14 17:23:47 +0530

    STATICFILES_STORAGE

--- config/settings/production.py
+++ config/settings/production.py
@@ -81,7 +81,9 @@ INSTALLED_APPS += ["storages"]  # noqa: F405
 # aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "shop.utils.storages.MediaRootS3Boto3Storage"
